0b0fd7dfdbe85dd337970292efa402c7e2081deb
Michael Wickey <wickeym@gmail.com>
2016-06-03 15:41:02 -0400

    updated to correct license format in setup

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-fastsettings',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
-    license='Apache 2.0 License',  # example license
+    license='Apache 2.0',
     description='A simple Django app to store/access project wide persistent settings.',
     long_description=README,
     url='https://github.com/wickeym/django-fastsettings',
@@ -21,15 +21,16 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.9',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 1.9',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache 2.0 License',  # example license
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
